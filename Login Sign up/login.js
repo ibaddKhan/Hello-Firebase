c9926a49ed71dbf0bf84ef0967c825cd3b42aa0c
Ibad <136696616+ibaddKhan@users.noreply.github.com>
2023-11-01 03:23:27 +0500

    Done

--- Login Sign up/login.js
+++ Login Sign up/login.js
@@ -1,19 +1,18 @@
 import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js";
 import { auth } from "./config.js";
 
-const pass = document.querySelector(".pass");
-const email = document.querySelector(".email");
+const passs = document.querySelector(".pass");
+const emaill = document.querySelector(".email");
+
 const form = document
   .querySelector("form")
   .addEventListener("submit", async (e) => {
     e.preventDefault();
+    const pass = passs.value;
+    const email = emaill.value;
 
     try {
-      const res = await signInWithEmailAndPassword(
-        auth,
-        email.value,
-        pass.value
-      );
+      const res = await signInWithEmailAndPassword(auth, email, pass);
       console.log(res.user);
       console.log("Successfully Logged in");
     } catch (error) {
