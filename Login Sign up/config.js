35264db97a3d41350dd675596f8ff300ea8f386a
Ibad <136696616+ibaddKhan@users.noreply.github.com>
2023-10-31 23:54:21 +0500

    config file added

---
+++ Login Sign up/config.js
@@ -0,0 +1,10 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-app.js";
+const firebaseConfig = {
+  apiKey: "AIzaSyDtr2yN0ab6cO6llzMpnTPsaccp076-IVE",
+  authDomain: "login-signup-practice-84d0e.firebaseapp.com",
+  projectId: "login-signup-practice-84d0e",
+  storageBucket: "login-signup-practice-84d0e.appspot.com",
+  messagingSenderId: "442961636433",
+  appId: "1:442961636433:web:35e34d71b8431c6c92dc70",
+};
+const app = initializeApp(firebaseConfig);
