a0b994c7f11021f0ff5938532d349b469fc03040
Ibad <136696616+ibaddKhan@users.noreply.github.com>
2023-11-01 08:38:58 +0500

    Monkey window removed and nightmode added

--- Login Sign up/index.js
+++ Login Sign up/index.js
@@ -1,15 +1,49 @@
-setTimeout(function () {
-  const monkeyWindow = document.getElementById("monkey-window");
-  monkeyWindow.classList.remove("hidden");
+const nightModeToggle = document.getElementById("darkModeToggle");
+const toggleContainer = document.getElementById("toggleContainer");
+const toggleButton = document.getElementById("toggleButton");
+const body = document.body;
+const head = document.querySelector(".head");
 
-  const loginButton = document.getElementById("login-button");
-  const registerButton = document.getElementById("register-button");
+function toggleNightMode() {
+  body.classList.toggle("dark");
 
-  loginButton.addEventListener("click", function () {
-    window.location.href = "login.html";
-  });
+  const isDarkMode = body.classList.contains("dark");
 
-  registerButton.addEventListener("click", function () {
-    window.location.href = "signup.html";
-  });
-}, 2000);
+  if (isDarkMode) {
+    toggleContainer.classList.add("left-100");
+    toggleButton.style.transform = "translateX(100%)";
+    removeBoxShadow();
+    body.classList.add("dark");
+    head.classList.add("text-gray-800");
+  } else {
+    body.classList.remove("dark");
+    addBoxShadow();
+    toggleContainer.classList.remove("left-4");
+    toggleButton.style.transform = "translateX(0)";
+  }
+}
+function addBoxShadow() {
+  body.classList.add("shadow-md");
+}
+function removeBoxShadow() {
+  body.classList.remove("shadow-md");
+}
+
+nightModeToggle.addEventListener("change", toggleNightMode);
+document.addEventListener("DOMContentLoaded", function () {
+  setTimeout(function () {
+    const monkeyWindow = document.getElementById("monkey-window");
+    monkeyWindow.classList.remove("hidden");
+
+    const loginButton = document.getElementById("login-button");
+    const registerButton = document.getElementById("register-button");
+
+    loginButton.addEventListener("click", function () {
+      window.location.href = "login.html";
+    });
+
+    registerButton.addEventListener("click", function () {
+      window.location.href = "signup.html";
+    });
+  }, 2000);
+});
--- Login Sign up/login.js
+++ Login Sign up/login.js
@@ -1,40 +1,44 @@
 import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js";
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js";
 import { auth } from "./config.js";
 
 const passs = document.querySelector(".pass");
 const emaill = document.querySelector(".email");
 
-const form = document
-  .querySelector("form")
-  .addEventListener("submit", async (e) => {
-    e.preventDefault();
-    const pass = passs.value;
-    const email = emaill.value;
+const form = document.querySelector("form");
+const monkeyWindow = document.getElementById("monkey-window");
+const loginButton = document.getElementById("login-button");
+const registerButton = document.getElementById("register-button");
 
-    try {
-      const res = await signInWithEmailAndPassword(auth, email, pass);
-      console.log(res.user);
-      Swal.fire({
-        position: "top-center",
-        icon: "success",
-        title: "Succesfully Logged in",
-        showConfirmButton: false,
-        timer: 1500,
-      });
-      setTimeout(() => {
-        window.location = "./index.html";
-      }, 1000);
-    } catch (error) {
-      const errorCode = error.code;
-      Swal.fire({
-        position: "top-center",
-        icon: "error",
-        title: errorCode,
-        showConfirmButton: false,
-        timer: 1500,
-      });
-    }
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const pass = passs.value;
+  const email = emaill.value;
 
-    emaill.value = "";
-    passs.value = "";
-  });
+  try {
+    const res = await signInWithEmailAndPassword(auth, email, pass);
+    console.log(res.user);
+    Swal.fire({
+      position: "top-center",
+      icon: "success",
+      title: "Successfully Logged in",
+      showConfirmButton: false,
+      timer: 1500,
+    });
+    setTimeout(() => {
+      window.location = "./index.html";
+    }, 1000);
+  } catch (error) {
+    const errorCode = error.code;
+    Swal.fire({
+      position: "top-center",
+      icon: "error",
+      title: errorCode,
+      showConfirmButton: false,
+      timer: 1500,
+    });
+  }
+
+  emaill.value = "";
+  passs.value = "";
+});
