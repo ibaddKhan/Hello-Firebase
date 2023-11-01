5b18e05788e1410df6b93deec3a249b290e906ef
Ibad <136696616+ibaddKhan@users.noreply.github.com>
2023-11-01 09:30:21 +0500

    Added

--- Login Sign up/index.js
+++ Login Sign up/index.js
@@ -3,6 +3,8 @@ const toggleContainer = document.getElementById("toggleContainer");
 const toggleButton = document.getElementById("toggleButton");
 const body = document.body;
 const head = document.querySelector(".head");
+const nav = document.querySelector(".nav");
+const para = document.querySelectorAll(".para");
 
 function toggleNightMode() {
   body.classList.toggle("dark");
@@ -12,21 +14,42 @@ function toggleNightMode() {
   if (isDarkMode) {
     toggleContainer.classList.add("left-100");
     toggleButton.style.transform = "translateX(100%)";
-    removeBoxShadow();
     body.classList.add("dark");
+    addBackgroundToNav();
     head.classList.add("text-gray-800");
+    addBackgroundToPara();
+    removeBoxShadow();
   } else {
-    body.classList.remove("dark");
     addBoxShadow();
+    removeBackgroundFromNav();
     toggleContainer.classList.remove("left-4");
     toggleButton.style.transform = "translateX(0)";
+    body.classList.remove("dark");
+    removeBackgroundFromPara();
   }
 }
+
 function addBoxShadow() {
   body.classList.add("shadow-md");
 }
-function removeBoxShadow() {
-  body.classList.remove("shadow-md");
+
+function addBackgroundToPara() {
+  para.forEach((element) => {
+    element.classList.add("bg-gray-200");
+  });
+}
+
+function removeBackgroundFromPara() {
+  para.forEach((element) => {
+    element.classList.remove("bg-gray-200");
+  });
+}
+function addBackgroundToNav() {
+  nav.classList.add("bg-violet-700");
+}
+
+function removeBackgroundFromNav() {
+  nav.classList.remove("bg-violet-700");
 }
 
 nightModeToggle.addEventListener("change", toggleNightMode);
