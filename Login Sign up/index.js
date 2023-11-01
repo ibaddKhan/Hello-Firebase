816975f4725d267f57f947b065c08888622b25de
Ibad <136696616+ibaddKhan@users.noreply.github.com>
2023-11-01 08:40:14 +0500

    removed monkey window

--- Login Sign up/index.js
+++ Login Sign up/index.js
@@ -30,20 +30,3 @@ function removeBoxShadow() {
 }
 
 nightModeToggle.addEventListener("change", toggleNightMode);
-document.addEventListener("DOMContentLoaded", function () {
-  setTimeout(function () {
-    const monkeyWindow = document.getElementById("monkey-window");
-    monkeyWindow.classList.remove("hidden");
-
-    const loginButton = document.getElementById("login-button");
-    const registerButton = document.getElementById("register-button");
-
-    loginButton.addEventListener("click", function () {
-      window.location.href = "login.html";
-    });
-
-    registerButton.addEventListener("click", function () {
-      window.location.href = "signup.html";
-    });
-  }, 2000);
-});
